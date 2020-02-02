06682415369bbe5b30905ed0e696526ce13f4288
Shazwi Suwandi <supershazwi@gmail.com>
2020-02-02 22:41:14 +0800

    added Procfile

--- app.js
+++ app.js
@@ -33,9 +33,7 @@ app.get('/qb-callback', (req, res) => {
 
 app.get('/', (req, res) =>  {
 	if(oauthClient.isAccessTokenValid()) {
-		console.log(oauthClient);
-
-		res.render('show-credentials', { access_token: oauthClient.access_token, refresh_token: oauthClient.refresh_token });
+		res.render('show-credentials', { access_token: oauthClient.token.access_token, refresh_token: oauthClient.token.refresh_token });
 	} 
 
 	if(!oauthClient.isAccessTokenValid()){
