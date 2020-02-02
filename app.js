197e80ede2bce6a290d53a074bde5b51f062b08c
Shazwi Suwandi <supershazwi@gmail.com>
2020-02-02 23:08:00 +0800

    update qb

--- app.js
+++ app.js
@@ -24,7 +24,13 @@ app.get('/qb-callback', (req, res) => {
     .then(function(authResponse) {
     	// store qb credentials
 
-    	res.render('show-credentials', { access_token: authResponse.getJson().access_token, refresh_token: authResponse.getJson().refresh_token });
+    	res.render('show-credentials', { 
+    		access_token: authResponse.getJson().access_token, 
+    		refresh_token: authResponse.getJson().refresh_token,  
+    		token_type: authResponse.getJson().token_type, 
+    		expires_in: authResponse.getJson().expires_in,
+    		x_refresh_token_expires_in: authResponse.getJson().x_refresh_token_expires_in
+    	});
     })
     .catch(function(e) {
         res.send("The error message is :"+e.originalMessage);
@@ -33,7 +39,13 @@ app.get('/qb-callback', (req, res) => {
 
 app.get('/', (req, res) =>  {
 	if(oauthClient.isAccessTokenValid()) {
-		res.render('show-credentials', { access_token: oauthClient.token.access_token, refresh_token: oauthClient.token.refresh_token });
+		res.render('show-credentials', { 
+			access_token: oauthClient.token.access_token, 
+			refresh_token: oauthClient.token.refresh_token,
+			token_type: oauthClient.token.token_type, 
+			expires_in: oauthClient.token.expires_in,
+			x_refresh_token_expires_in: oauthClient.token.x_refresh_token_expires_in
+		});
 	} 
 
 	if(!oauthClient.isAccessTokenValid()){
